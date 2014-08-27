e11bb4f59de0469dc18be52a0671572347a2c85c
kristoph <github.com@kristoph.net>
2014-08-27 15:21:01 -0700

    implemented various query criteria methods including where, order, limit and offset

--- lib/boxxspring/operation.rb
+++ lib/boxxspring/operation.rb
@@ -2,20 +2,50 @@ module Boxxspring
 
   class Operation 
 
-    def initialize( path )
+    def initialize( path, parameters = {}, result = Array )
       @path = path 
-      @parameters = {}
+      @parameters = ( parameters || {} ).deep_dup
+    end
+
+    def where( parameters )
+      self.spawn( parameters )
+    end
+
+    def order( by, direction = 'desc' )
+      self.spawn( sort_by: by, sort_direction: direction )
+    end
+
+    def limit( _limit )
+      self.spawn( count: _limit )
+    end
+
+    def offset( _offset )
+      self.spawn( offset: _offset )
     end
 
     def query
       result = nil
       Boxxspring::Request.new.tap do | request |
-        request.get( @path ).tap do | response |
+        request.get( @path, @parameters ).tap do | response |
           parser = Boxxspring::Parser.new( response.content )
           result = parser.resources
+          result = result.first if result.length > 0 && @result == Object 
         end
       end
       result
+    end
+
+    def read 
+      result = self.query
+      result = result.first if result.present? && result.is_a?( Enumerable )
+      result
+    end
+
+    protected; def spawn( parameters  )
+      Boxxspring::Operation.new( 
+        @path,
+        @parameters.deep_merge( parameters || {} )
+      )
     end
 
   end
--- lib/boxxspring/version.rb
+++ lib/boxxspring/version.rb
@@ -1,3 +1,3 @@
 module Boxxspring
-	VERSION = '2.0.3'
+	VERSION = '2.0.5'
 end
