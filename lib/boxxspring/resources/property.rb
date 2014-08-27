7f7cd4760c2d3fb4517092722ee10e970c38b389
kristoph <github.com@kristoph.net>
2014-08-27 14:42:06 -0700

    added type_name and id fields; added operation method for property operations

--- lib/boxxspring/resources/property.rb
+++ lib/boxxspring/resources/property.rb
@@ -2,23 +2,31 @@ module Boxxspring
 
 	class Property < Base
 
-	  field  :created_at
-	  field  :updated_at
-	  field  :destroyed_at
+    field   :type_name
+    field   :id
 
-	  field  :name
-	  field  :code_name
-	  field  :domain_name
+	  field   :created_at
+	  field   :updated_at
+	  field   :destroyed_at
 
-	  field  :meta_description
-	  field  :meta_title
+	  field   :name
+	  field   :code_name
+	  field   :domain_name
 
-    field  :authentication_enabled,         default: false
-    field  :authentication_username
-    field  :authentication_password
+	  field   :meta_description
+	  field   :meta_title
 
+    field   :authentication_enabled,         default: false
+    field   :authentication_username
+    field   :authentication_password
+
+    has_many  :groups
     has_many  :pictures
     has_one   :theme
+
+    def operation( path )
+      Boxxspring::Operation.new( File.join( '/properties', self.id.to_s, path ) )
+    end
 
 	end
 
