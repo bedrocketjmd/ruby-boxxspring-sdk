2a39a3986479155311a06c79f4f8b471fd775760
Carolyn Ganon <carolyn@Carolyn.local>
2014-10-20 13:53:02 -0400

    add video model

---
+++ lib/boxxspring/resources/video.rb
@@ -0,0 +1,15 @@
+module Boxxspring
+
+  class Video < Base
+
+    field  :created_at
+    field  :updated_at
+
+    field  :id
+    field  :storage_key
+    field  :filename
+    field  :output_sources
+    
+  end
+
+end
