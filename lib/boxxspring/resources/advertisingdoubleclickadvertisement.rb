9f76850964895088b39bd5c3b4e84d24be1144a0
Carolyn Gannon <cganon@gmail.com>
2014-12-12 13:42:20 -0500

    add advertising to gem

---
+++ lib/boxxspring/resources/advertisingdoubleclickadvertisement.rb
@@ -0,0 +1,10 @@
+module Boxxspring
+  class AdvertisingDoubleClickAdvertisement < Base; 
+    field  :type_name 
+    field  :name
+    field  :state
+    field  :iu
+    field  :sz
+    field  :tag_url
+  end
+end
