bfbbeb0324bbf76672db84c6f57fb5e90fb8d04c
Carolyn Gannon <cganon@gmail.com>
2014-12-16 20:27:07 -0500

    fix default advertisement field in property

--- lib/boxxspring/resources/property.rb
+++ lib/boxxspring/resources/property.rb
@@ -19,14 +19,12 @@ module Boxxspring
     field   :authentication_username
     field   :authorization_password
 
-    field   :default_advertisement_id
-
     has_many  :groups
     has_many  :pages
     has_many  :services
     has_many  :pictures
     has_one   :theme
-    has_one   :default_advertisements
+    has_one   :default_advertisement
 
     def operation( path )
       Boxxspring::Operation.new( File.join( '/properties', self.id.to_s, path ) )
