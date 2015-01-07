b35ac7f579e1ff7166c8e15a1818eb5c9c8fa4ea
Kevin Merritt <kevinwmerritt@gmail.com>
2015-01-07 11:50:34 -0500

    update video_source_segment

--- lib/boxxspring/resources/video_source.rb
+++ lib/boxxspring/resources/video_source.rb
@@ -5,13 +5,15 @@ module Boxxspring
     field  :created_at
     field  :updated_at
 
+    field  :id
+    field  :video_id
     field  :storage_key
     field  :name
     field  :content_type
     field  :width
     field  :height
 
-    has_one :video
+    has_many :segments
 
   end
 
--- lib/boxxspring/resources/video_source_segment.rb
+++ lib/boxxspring/resources/video_source_segment.rb
@@ -5,13 +5,13 @@ module Boxxspring
     field  :created_at
     field  :updated_at
 
+    field  :id
+    field  :video_source_id
     field  :storage_key
     field  :name
     field  :content_type
     field  :width
     field  :height
-
-    has_one :video_source
 
   end
 
