98429d5f48ab5d1933db423bf35ed2165f566522
Kevin Merritt <kevinwmerritt@gmail.com>
2015-01-08 11:38:25 -0500

    videos ingestion task

---
+++ lib/boxxspring/resources/videos_ingestion_task.rb
@@ -0,0 +1,17 @@
+module Boxxspring
+
+  class VideosIngestionTask < Task
+
+    field   :name
+    field   :description
+    field   :tags
+    field   :originated_at
+    field   :note
+    field   :provider_uid
+    field   :provider_url
+    field   :video_file_url
+    field   :picture_file_url
+
+  end
+
+end
