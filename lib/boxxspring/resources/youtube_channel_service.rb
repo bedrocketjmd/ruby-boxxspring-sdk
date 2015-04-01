15cc3d0a95f1e20ed388c693e992f141148787dc
Kevin Merritt <kevinwmerritt@gmail.com>
2015-04-01 12:27:09 -0400

    include content owner in youtube channel service

--- lib/boxxspring/resources/youtube_channel_service.rb
+++ lib/boxxspring/resources/youtube_channel_service.rb
@@ -3,6 +3,9 @@ module Boxxspring
   class YoutubeChannelService < SocialService 
     field :refresh_token
     field :description
+    field :thumbnail_url
+    field :content_owner_id
+    field :content_owner_display_name
   end
 
 end
