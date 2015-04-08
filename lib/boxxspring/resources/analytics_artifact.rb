e9dfd2ef5d640f0c052b283f1ff43a2a8261c1ce
Jaclyn <jaclyn@bedrocket.com>
2015-04-08 16:04:13 -0400

    add analytics artifact resource

---
+++ lib/boxxspring/resources/analytics_artifact.rb
@@ -0,0 +1,43 @@
+module Boxxspring
+  class AnalyticsArtifact < Base
+
+    field     :id
+
+    field     :created_at
+    field     :updated_at
+
+    field     :type_name
+
+    field     :at
+    field     :participant_id
+    field     :artifact_id 
+    field     :context_uuid
+    field     :session_uuid
+    field     :ip_address
+    field     :user_agent
+    field     :device_type
+    field     :device_name
+    field     :operating_system_name
+    field     :operating_system_version
+    field     :user_agent_name
+    field     :user_agent_version
+    field     :geography_country
+    field     :geography_locality
+    field     :referer_url
+    field     :referer_type
+    field     :time_total
+    field     :interation_time_total
+    field     :interaction_count
+    field     :play_count
+    field     :play_time_total
+    field     :play_time_maximum
+    field     :play_time_maximum_percentage
+    field     :pause_count
+    field     :pause_time_total
+    field     :wait_count
+    field     :wait_time_total
+    field     :ad_count
+    field     :ad_time_total
+
+  end
+end
