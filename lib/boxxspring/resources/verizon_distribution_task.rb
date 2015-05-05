3dfa3ef191e8a1b4fe1cf81b1cf53f5b8248c0bc
zerogee00 <david@bedrocket.com>
2015-05-05 18:57:23 -0400

    verizon distribution and task

---
+++ lib/boxxspring/resources/verizon_distribution_task.rb
@@ -0,0 +1,12 @@
+module Boxxspring
+
+  class VerizonDistributionTask < Task
+
+  	field :distribution_id
+    field :video_title
+    field :video_description
+    field :episode_number
+    field :closed_captions
+    
+  end
+end
