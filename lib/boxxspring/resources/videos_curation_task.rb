8602feb51a1139408412197042ae43b1d296686d
Jaclyn <jaclyn@bedrocket.com>
2015-05-09 12:47:24 -0400

    add videos curation task to sdk

--- lib/boxxspring/resources/videos_curation_task.rb
+++ lib/boxxspring/resources/videos_curation_task.rb
@@ -9,6 +9,7 @@ module Boxxspring
     field   :video_provider_url
     field   :video_originated_at
     field   :video_originated_at
+    field   :provider_url
     field   :provider
     field   :attribution_name
     field   :attribution_provider_uid
