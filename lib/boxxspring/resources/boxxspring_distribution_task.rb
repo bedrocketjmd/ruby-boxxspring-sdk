b87145af6183e16afa4de4a618cebcecc8b348b1
David Bragdon <davidbragdon@bedrocket.com>
2015-08-07 11:15:38 -0400

    Remove short_name, short_description, note, seo_title, seo_description, and url_slug from BoxxspringDistributionTask fields; add published

--- lib/boxxspring/resources/boxxspring_distribution_task.rb
+++ lib/boxxspring/resources/boxxspring_distribution_task.rb
@@ -5,12 +5,7 @@ module Boxxspring
     field :distribution_id
     field :name
     field :description
-    field :short_name
-    field :short_description
-    field :note
-    field :seo_title
-    field :seo_description
-    field :url_slug
+    field :published
     field :artifact_ids
 
   end
