81dc1a721fc23a2f4a1b0276387692290b641057
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-08-12 20:09:10 +0200

    Add missing field to bxx distribution

--- lib/boxxspring/resources/boxxspring_distribution.rb
+++ lib/boxxspring/resources/boxxspring_distribution.rb
@@ -2,8 +2,9 @@ module Boxxspring
 
   class BoxxspringDistribution < Distribution
 
-    field :property_name
     field :destination_property_id
+    field :property_name
+    field :property_logo
 
   end
 
