a02a55280638186ff2c1be768b8544779545ed4b
zerogee00 <david@bedrocket.com>
2015-10-16 20:19:48 -0400

    add fields to token and stream

--- lib/boxxspring/resources/stream.rb
+++ lib/boxxspring/resources/stream.rb
@@ -2,6 +2,15 @@ module Boxxspring
 
   class Stream < Base
 
+    field     :id
+    field     :name
+    field     :created_at
+    field     :updated_at
+    field     :recording
+    field     :state
+    field     :artifact_id
+    field     :property_id
+
   end
 
 end
--- lib/boxxspring/resources/token.rb
+++ lib/boxxspring/resources/token.rb
@@ -2,6 +2,14 @@ module Boxxspring
 
   class Token < Base
 
+  	 	field 			:id
+      field 			:uuid
+      field 			:type_name
+      field 			:stream_id
+      field 			:created_at
+      field 			:updated_at
+      field 			:property_id
+
   end
 
 end
