e4655310d4b66fb7b91b3c71821e7992c6081cd4
zerogee00 <david@bedrocket.com>
2015-10-23 16:18:14 -0400

    fix formatting token

--- lib/boxxspring/resources/token.rb
+++ lib/boxxspring/resources/token.rb
@@ -2,13 +2,13 @@ module Boxxspring
 
   class Token < Base
 
-  	 	field 			:id
-      field 			:uuid
-      field 			:type_name
-      field 			:stream_id
-      field 			:created_at
-      field 			:updated_at
-      field 			:property_id
+      field       :id
+      field       :uuid
+      field       :type_name
+      field       :stream_id
+      field       :created_at
+      field       :updated_at
+      field       :property_id
 
   end
 
