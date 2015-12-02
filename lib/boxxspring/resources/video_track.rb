73e6e438e153e8e3d33401dff582237ca80c5452
kristoph <git@kristoph.net>
2015-12-02 10:44:05 -0800

    add VideoText and VideoTrack resources; modified video to add; bumped version

---
+++ lib/boxxspring/resources/video_track.rb
@@ -0,0 +1,23 @@
+module Boxxspring
+
+  class VideoTrack < Base
+
+    field   :created_at
+    field   :updated_at
+
+    field   :id
+    field   :filename
+    field   :storage_key
+    field   :size
+    field   :content_type
+
+    field   :kind
+    field   :language
+    field   :label
+
+    field   :video_id 
+    field   :video_text_id 
+
+  end
+
+end
--- lib/boxxspring/version.rb
+++ lib/boxxspring/version.rb
@@ -1,3 +1,3 @@
 module Boxxspring
-  VERSION = '2.4.8'
+  VERSION = '2.4.9'
 end
