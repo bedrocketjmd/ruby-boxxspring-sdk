290e30273653a6292be77a2e8597e50a3e8c1b9e
David Bragdon <davidbragdon@bedrocket.com>
2016-03-07 11:42:31 -0500

    Add DailymotionDistributionOperation, VerizonDistributionOperation, BoxxspringDistributionOperation, YoutubeDistributionOperation, and FacebookPageDistributionOperation

--- lib/boxxspring/resources/verizon_distribution_operation.rb
+++ lib/boxxspring/resources/verizon_distribution_operation.rb
@@ -1,8 +1,9 @@
 module Boxxspring
 
   class VerizonDistributionOperation < DistributionOperation
-
-
+    field  :video_title
+    field  :video_description
+    field  :episode_number
   end
 
 end
