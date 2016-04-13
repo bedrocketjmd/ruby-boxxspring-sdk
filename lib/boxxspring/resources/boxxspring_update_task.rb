8b6c7b504918db9b8a9b985bd021a1bb0f6c41d0
David Bragdon <davidbragdon@bedrocket.com>
2016-04-13 10:28:20 -0400

    Add BoxxspringUpdateTask—and require it

---
+++ lib/boxxspring/resources/boxxspring_update_task.rb
@@ -0,0 +1,13 @@
+module Boxxspring
+
+  class BoxxspringUpdateTask < Task
+
+    field :artifact_state
+    field :distribution_id
+    field :name
+    field :description
+    field :artifact_ids
+
+  end
+
+end
