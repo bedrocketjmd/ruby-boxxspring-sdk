c2b523aef5825dc85e9d9f7705103d761b366aa9
David Bragdon <davidbragdon@bedrocket.com>
2016-05-26 10:53:57 -0400

    Add new "properties" fields to Boxxspring Distribution and Update Activity and Task

--- lib/boxxspring/resources/boxxspring_distribution_activity.rb
+++ lib/boxxspring/resources/boxxspring_distribution_activity.rb
@@ -2,10 +2,18 @@ module Boxxspring
 
   class BoxxspringDistributionActivity < Activity
 
-    field  :name
-    field  :description
-    field  :published
-    field  :artifact_ids
+    field :name
+    field :description
+    field :published
+    field :artifact_ids
+    field :short_description
+    field :slug
+    field :meta_description
+    field :meta_title
+    field :note
+    field :short_name
+    field :attribution_id
+    field :picture_id
 
   end
 
