ffc5e88e1ba5a116575681fb4a90aa298971362c
David Bragdon <davidbragdon@bedrocket.com>
2016-07-12 16:56:45 -0400

    Add post_video field to TwitterDistributionActivity/Task

--- lib/boxxspring/resources/twitter_distribution_activity.rb
+++ lib/boxxspring/resources/twitter_distribution_activity.rb
@@ -2,6 +2,7 @@ module Boxxspring
 
   class TwitterDistributionActivity < Activity
     field  :tweet_status
+    field  :post_video
   end
 
 end
--- lib/boxxspring/resources/twitter_distribution_task.rb
+++ lib/boxxspring/resources/twitter_distribution_task.rb
@@ -4,6 +4,7 @@ module Boxxspring
 
     field   :distribution_id
     field   :tweet_status
+    field   :post_video
 
   end
 
