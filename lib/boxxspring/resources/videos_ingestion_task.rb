c86acba281d69d02a8da28416c553df76a143f91
jaclynj <jaclyn@bedrocket.com>
2016-10-28 13:40:37 -0400

    add field artifact_type_name to videos ingestion task

--- lib/boxxspring/resources/videos_ingestion_task.rb
+++ lib/boxxspring/resources/videos_ingestion_task.rb
@@ -16,6 +16,7 @@ module Boxxspring
     field   :alternate_provider_url
     field   :auto_publish
     field   :artifact_id
+    field   :artifact_type_name
     field   :categories
     field   :category_ids
 
