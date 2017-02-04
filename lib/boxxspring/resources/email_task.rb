6a37262acb3044f7da84a1c0d2f64a387a5354ea
Matthew Yang <yang70@gmail.com>
2017-02-04 06:23:35 -0800

    add category_type and artifact_type fields to email task resource

--- lib/boxxspring/resources/email_task.rb
+++ lib/boxxspring/resources/email_task.rb
@@ -5,8 +5,10 @@ module Boxxspring
     field  :email_type
     field  :email_address
     field  :category_name
+    field  :category_type
     field  :name
     field  :artifact_id
+    field  :artifact_type
 
   end
 
