4aea67c162cf3d814dd5813871196b161e425480
Susan Prestage <susan@sportsrocket.com>
2017-02-14 15:58:33 -0800

    Add task and activity to support facebook stream distribution.

--- lib/boxxspring/resources/facebook_stream_distribution_activity.rb
+++ lib/boxxspring/resources/facebook_stream_distribution_activity.rb
@@ -1,7 +1,9 @@
 module Boxxspring
 
   class FacebookStreamDistributionActivity < Activity
-
+    field   :post_name
+    field   :post_message
+    field   :post_stream_description
   end
 
 end
--- lib/boxxspring/resources/facebook_stream_distribution_task.rb
+++ lib/boxxspring/resources/facebook_stream_distribution_task.rb
@@ -1,6 +1,9 @@
 module Boxxspring
 
   class FacebookStreamDistributionTask < Task
+    field   :post_name
+    field   :post_message
+    field   :post_stream_description
     field   :distribution_id
   end
 
