8204dcf842daa5e9f1ee243b001cd32c7c2805d0
jacksonsouza <jacksonsouza@me.com>
2017-04-03 10:17:59 -0700

    yt deletion work

---
+++ lib/boxxspring/resources/youtube_distribution_deletion_activity.rb
@@ -0,0 +1,7 @@
+module Boxxspring
+
+  class FacebookDistributionDeletionActivity < Activity
+    field :distribution_artifact_locator_id
+  end
+
+end
---
+++ lib/boxxspring/resources/youtube_distribution_deletion_task.rb
@@ -0,0 +1,8 @@
+module Boxxspring
+
+  class FacebookDistributionDeletionTask < Task
+    field   :distribution_id
+    field   :distribution_artifact_locator_id
+  end
+
+end
