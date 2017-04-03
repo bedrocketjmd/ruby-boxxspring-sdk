e895b4e44a5de0f8196ea91666e3da9fc95a9389
jacksonsouza <jacksonsouza@me.com>
2017-04-03 14:12:26 -0700

    added yt ids

--- lib/boxxspring/resources/youtube_distribution_deletion_activity.rb
+++ lib/boxxspring/resources/youtube_distribution_deletion_activity.rb
@@ -1,7 +1,9 @@
 module Boxxspring
 
   class YoutubeDistributionDeletionActivity < Activity
-    field :distribution_artifact_locator_id
+   field   :youtube_broadcast_id
+   field   :youtube_stream_id 
+   field   :distribution_artifact_locator_id
   end
 
 end
--- lib/boxxspring/resources/youtube_distribution_deletion_task.rb
+++ lib/boxxspring/resources/youtube_distribution_deletion_task.rb
@@ -2,6 +2,8 @@ module Boxxspring
 
   class YoutubeDistributionDeletionTask < Task
     field   :distribution_id
+    field   :youtube_broadcast_id
+    field   :youtube_stream_id
     field   :distribution_artifact_locator_id
   end
 
